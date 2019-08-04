b867ee2f46f95ad20f7afa0b10b3517a725d0ae7
joseaugustodemelo <joseaugustodemelo@joseaugustodemelo>
2019-08-04 17:43:22 -0300

    JAVA 8 STREAMS: Pare de usar ‘for’ e simplifique seu código! - Parte 2

--- Java8Lambas/src/main/java/java8lambdas/Java8Streams.java
+++ Java8Lambas/src/main/java/java8lambdas/Java8Streams.java
@@ -4,7 +4,11 @@
 package java8lambdas;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Java8Streams {
 
@@ -56,6 +60,67 @@ public class Java8Streams {
 			.map(e -> e * 2) // Transformando os elementos. NÃO ALTERA A LISTA ORIGINAL
 			.forEach(e -> System.out.println(e));
 		
+		System.out.println("");
+		
+		// count
+		System.out.println("Exemplo de Stream com Count.");
+		long count = lista.stream()
+			.filter(e -> e % 2 == 0) 
+			.count(); // Método terminal que retorna a contagem de elementos
+		System.out.println(count);
+		
+		System.out.println("");
+		
+		// min
+		System.out.println("Exemplo de Stream com Min.");
+		Optional<Integer> min = lista.stream()
+			.min(Comparator.naturalOrder()); // Método terminal que retorna Menor elemento
+		System.out.println(min.get());
+		
+		System.out.println("");
+		
+		// max
+		System.out.println("Exemplo de Stream com Max.");
+		Optional<Integer> max = lista.stream()
+			.max(Comparator.naturalOrder()); // Método terminal que retorna Máximo elemento
+		System.out.println(max.get());
+		
+		System.out.println("");
+		
+		// collect
+		System.out.println("Exemplo de Stream com Collect.");
+		List<Integer> novaLista = lista.stream()
+			.filter(e -> e % 2 == 0)
+			.collect(Collectors.toList()); // Método terminal que retorna um coleção de elementos
+		
+		System.out.println(novaLista);
+		
+		System.out.println("");
+		
+		// collect
+		System.out.println("Exemplo de Stream com Collect.");
+		Map<Boolean, List<Integer>> mapa = lista.stream()
+			.map(e -> e * 3)
+			.collect(Collectors.groupingBy(e -> e % 2 == 0)); // Método terminal que retorna um coleção de elementos de forma agrupada
+		
+		System.out.println(mapa);
+		
+		// collect
+		System.out.println("Exemplo de Stream com Collect.");
+		Map<Integer, List<Integer>> mapaInteger = lista.stream()
+			.collect(Collectors.groupingBy(e -> e % 2)); // Método terminal que retorna um coleção de elementos de forma agrupada
+		
+		System.out.println(mapaInteger);
+		
+		System.out.println("");
+		
+		// collect
+		System.out.println("Exemplo de Stream com Collect.");
+		String collect = lista.stream()
+			.map(e -> String.valueOf(e))
+			.collect(Collectors.joining(";")); // Método terminal que retorna um coleção de elementos de forma agrupada utilizando Strings
+		
+		System.out.println(collect);
 	}
 
 }
